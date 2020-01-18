f9250d57d065c114849e792536d7602f758955da
Alexey <manesio2009@gmail.com>
2020-01-19 01:26:46 +0200

    Change script

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
  let money = prompt('Ваш бюджет на месяц?');
  let time = prompt('Введите дату в формате YYYY-MM-DD"');
+
  let appData = {
      money: money,
      timeData: time,
@@ -8,5 +9,16 @@
      income: [],
      savings: false
  };
+
+ q1 = prompt('Введите обязательную статью расходов в этом месяце');
+ a1 = prompt('Во сколько обойдется?');
+
+ appData.expenses.q1 = a1;
+
+ q2 = prompt('Введите обязательную статью расходов в этом месяце');
+ a2 = prompt('Во сколько обойдется?');
+
+ appData.expenses.q2 = a2;
+
 
 alert(appData.money/30);  
