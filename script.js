57744181ad5b1ddb9cf97fde6e2ca10858605db0
Alexey <manesio2009@gmail.com>
2020-01-22 19:16:57 +0200

    fourth homework

--- script.js
+++ script.js
@@ -17,96 +17,82 @@ let appData = {
     expenses: {},
     optionalExpenses: {},
     income: [],
-    savings: true
+    savings: true,
+    chooseExpenses: function() {
+        for (let i = 0; i < 2; i++) {
+            let q = prompt('Введите обязательную статью расходов в этом месяце'),
+                a = prompt('Во сколько обойдется?');
+        
+            if ((typeof (q) === 'string') && (typeof (q) != null) && (typeof (a) != null) &&
+                a != '' && q != '' && a.length < 50) {
+                appData.expenses[q] = a;
+            } else {
+                console.log("Написал дичь какую-то");
+                i--;
+            }
+        };
+    },
+    detectDayBudget: function() {
+        appData.moneyPerDay = (appData.money / 30).toFixed();
+        alert('Ежедневный бюджет: ' + appData.moneyPerDay);
+    },
+    detectLevel: function() {
+        if (appData.moneyPerDay < 100) {
+            console.log("Минимальный уровень достатка");
+        } else if (appData.moneyPerDay < 2000) {
+            console.log("Средний уровень достатка");
+        } else if (appData.moneyPerDay > 2000) {
+            console.log("Высокий уровень достатка");
+        } else {
+            console.log("Ошибка");
+        };
+    },
+    checkSavings: function() {
+        if (appData.savings) {
+            let save = +prompt("Какая сумма накоплений"),
+               percent = +prompt('Под какой процент?');
+   
+           appData.monthIncome = save/100/12*percent;
+           alert('Доход в месяц с депозита: ' + appData.monthIncome)
+        }
+    },
+    chooseOptExpenses: function() {
+        for (let i=0; i<3; i++) {
+            let q = prompt('Введите обязательную статью расходов в этом месяце'),
+                a = prompt('Во сколько обойдется?');
+        
+            if ((typeof (q) === 'string') && (typeof (q) != null) && (typeof (a) != null) &&
+                a != '' && q != '' && a.length < 50) {
+                appData.optionalExpenses[q] = a;
+            } else {
+                console.log("Написал дичь какую-то");
+                i--;
+            }
+        }
+    }, 
+    chooseIncome: function() {
+        let items = prompt('Что принесет дополнительный доход? (Перечислите через запятую)');
+
+        if ((typeof (items) === 'string') && (typeof (items) != null) && items != '' && items.length < 50) {
+            appData.income = items.split(',');
+        }
+        
+        appData.income.push(prompt('Может что-то еще?'));
+        appData.income.sort();
+
+        appData.income.forEach(function(elem, i) {
+            alert('Способы зароботка: ' + elem);
+        });
+
+        
+        console.log(" Наша программа включает в себя данные: ");
+        for (let elem in appData) {
+            console.log(elem);
+            
+        }
+        
+
+    }
 };
 
-function chooseExpenses() {
-    for (let i = 0; i < 2; i++) {
-        let q = prompt('Введите обязательную статью расходов в этом месяце'),
-            a = prompt('Во сколько обойдется?');
-    
-        if ((typeof (q) === 'string') && (typeof (q) != null) && (typeof (a) != null) &&
-            a != '' && q != '' && a.length < 50) {
-            appData.expenses[q] = a;
-        } else {
-            console.log("Написал дичь какую-то");
-            i--;
-        }
-    };
-}
-
-chooseExpenses();
-
-//  let i = 0;
-//  while (i < 2) {
-//      let q = prompt('Введите обязательную статью расходов в этом месяце'),
-//          a = prompt('Во сколько обойдется?');
-
-//      if ((typeof (q) === 'string') && (typeof (q) != null) && (typeof (a) != null) &&
-//          a != '' && q != '' && a.length < 50) {
-//          appData.expenses[q] = a;
-//      } else {
-//          console.log("Написал дичь какую-то");
-//      }
-//      i++;
-//  };
-
-//  let i = 0;
-//  do {
-//     let q = prompt('Введите обязательную статью расходов в этом месяце'),
-//          a = prompt('Во сколько обойдется?');
-
-//      if ((typeof (q) === 'string') && (typeof (q) != null) && (typeof (a) != null) &&
-//          a != '' && q != '' && a.length < 50) {
-//          appData.expenses[q] = a;
-//      } else {
-//          console.log("Написал дичь какую-то");
-//      }
-//      i++;
-//  }
-//  while (i < 2);
-
-
-function detectDayBudget() {                                                            //функция расчета дневного бюджета
-    appData.moneyPerDay = (appData.money / 30).toFixed();
-    alert('Ежедневный бюджет: ' + appData.moneyPerDay);
-}
-
-function detectLevel() {                                                                //функция определеия уровня достатка
-    if (appData.moneyPerDay < 100) {
-        console.log("Минимальный уровень достатка");
-    } else if (appData.moneyPerDay < 2000) {
-        console.log("Средний уровень достатка");
-    } else if (appData.moneyPerDay > 2000) {
-        console.log("Высокий уровень достатка");
-    } else {
-        console.log("Ошибка");
-    }
-}
-
-function checkSavings() {                                                               //функция определеия накоплений
-     if (appData.savings) {
-         let save = +prompt("Какая сумма накоплений"),
-            percent = +prompt('Под какой процент?');
-
-        appData.monthIncome = save/100/12*percent;
-        alert('Доход в месяц с депозита: ' + appData.monthIncome)
-     }
-}
-
-function chooseOptExpenses() {                                                          //функция определеия необязательных расходов
-    for (let i=0; i<3; i++) {
-        let q = prompt('Введите обязательную статью расходов в этом месяце'),
-            a = prompt('Во сколько обойдется?');
-    
-        if ((typeof (q) === 'string') && (typeof (q) != null) && (typeof (a) != null) &&
-            a != '' && q != '' && a.length < 50) {
-            appData.optionalExpenses[q] = a;
-        } else {
-            console.log("Написал дичь какую-то");
-            i--;
-        }
-    }
-}
-
-checkSavings();
+appData.chooseIncome();
